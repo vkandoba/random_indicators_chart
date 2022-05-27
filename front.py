07a2085e2f1705b4acb6199ae377ce90b6b273af
Viktor I. Kandoba <kavi@mtl.ru>
2022-05-27 15:32:35 +0300

    Reload and update plot on change instrument

--- front.py
+++ front.py
@@ -1,5 +1,9 @@
+import pandas as pd
+
 from requests import get
-from dash import Dash, html, dcc
+
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
 
 
 def instrument_symbol_list():
@@ -9,10 +13,24 @@ def instrument_symbol_list():
         return [instrument['symbol'] for instrument in instruments]
 
 
+def price_list(symbol):
+    price_response = get(f"http://127.0.0.1:5000/instrument/{symbol}/price")
+    if (price_response.ok):
+        prices = price_response.json()
+        return prices
+
+
+def create_price_figure(price_list):
+    df = pd.DataFrame({'Time': range(len(price_list)), 'Price': price_list})
+    return px.line(df, x="Time", y="Price")
+
+
 app = Dash(__name__)
 app.title = 'Test. Python'
 
 instruments = instrument_symbol_list()
+price_values = price_list(instruments[0])
+init_fig = create_price_figure(price_values)
 
 app.layout = html.Div(children=[
     html.H1(children='Chart'),
@@ -21,9 +39,25 @@ app.layout = html.Div(children=[
 
     html.Div(
         [
-            dcc.Dropdown(instruments, instruments[0], clearable=False)
-        ], style={"width": '15%'})
+            dcc.Dropdown(instruments, instruments[0], id='instrument-selector', clearable=False)
+        ], style={"width": '15%'}),
+
+    dcc.Graph(
+        id='price-graph',
+        figure=init_fig
+    )
 ])
+
+
+@app.callback(
+    Output(component_id='price-graph', component_property='figure'),
+    Input(component_id='instrument-selector', component_property='value')
+)
+def update_price_graph(instrument):
+    values = price_list(instrument)
+    fig = create_price_figure(values)
+    return fig
+
 
 if __name__ == '__main__':
     app.run(debug=True)
